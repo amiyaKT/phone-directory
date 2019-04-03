15f9555180f84c76587d2471fe9519546e8983f6
amiya-1998 <amiya.tripathy1998@gmail.com>
2019-04-03 08:14:27 +0530

    Added HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Landing from './Landing';
@@ -8,13 +8,13 @@ import AddContact from './AddContact';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <div className="container w-100">
           <Route exact path="/" component={Landing} />
           <Route exact path="/addContact" component={AddContact} />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
