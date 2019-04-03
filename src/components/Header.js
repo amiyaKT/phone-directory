3a44d3d2811084fa3e2443cf65507241a958a3a0
amiya-1998 <amiya.tripathy1998@gmail.com>
2019-04-03 09:03:50 +0530

    Minor styling changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-      <Link className="navbar-brand" to="/">
+      <Link className="navbar-brand w-100 text-center" to="/">
         Phone Directory
       </Link>
     </nav>
